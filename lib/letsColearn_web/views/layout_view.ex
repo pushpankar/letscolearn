0f95ec21ee9864de9dc5b6761175b3d1574b912a
Pushpankar <pushpankark@gmail.com>
2018-08-25 17:22:15 +0530

    Add login and logout support with UI

--- lib/letsColearn_web/views/layout_view.ex
+++ lib/letsColearn_web/views/layout_view.ex
@@ -1,3 +1,15 @@
 defmodule LetsColearnWeb.LayoutView do
   use LetsColearnWeb, :view
+
+  alias LetsColearn.Guardian
+
+  def login_or_logout(conn) do
+    maybe_user = Guardian.Plug.current_resource(conn)
+    if maybe_user do
+      render "logout.html", conn: conn
+    else
+      render "login.html", conn: conn
+    end
+
+  end
 end
