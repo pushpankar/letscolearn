e91b1844d7fda7803f237a9e0bd91e6e628b6cf0
Pushpankar <pushpankark@gmail.com>
2018-09-01 16:50:44 +0530

    Give guardian secret key

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,7 @@ config :letsColearn, LetsColearnWeb.Endpoint,
   load_from_system_env: true,
   url: [host: {:system, "HOST"}, port: {:system, "PORT"}],
   server: true,
-  version: Application.spec(:subs_web, :vsn),
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   session_cookie_name: System.get_env("SESSION_COOKIE_NAME"),
   session_cookie_signing_salt: System.get_env("SESSION_COOKIE_SIGNING_SALT"),
@@ -14,6 +14,14 @@ config :letsColearn, LetsColearnWeb.Endpoint,
 #   url: System.get_env("DATABASE_URL"),
 #   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
 #   ssl: false
+
+
+# Do not print debug messages in production
+config :logger, level: :info
+
+config :guardian, Guardian,
+  secret_key: System.get_env("GUARDIAN_SECRET_KEY")
+
 
 config :letsColearn, LetsColearn.Repo,
   adapter: Ecto.Adapters.Postgres,
